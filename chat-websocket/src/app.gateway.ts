6fd67d87d73f104a368eedcb4701cc59e0be8281
dlalstlr12 <as2984348@naver.com>
2024-06-04 15:56:57 +0900

    feat : 네임스페이스 사용 및 script 파일 분리

--- chat-websocket/src/app.gateway.ts
+++ chat-websocket/src/app.gateway.ts
@@ -5,7 +5,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({ namespace: 'chat' })
 export class ChatGateway {
   @WebSocketServer() server: Server;
 
